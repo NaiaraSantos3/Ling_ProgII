9333ecf9daf34a9c51a623f9996c087a0df98080
Naiara Santos <127265827+NaiaraSantos3@users.noreply.github.com>
2024-09-28 15:13:52 -0300

    Delete nsleilao directory

--- nsleilao/src/test/java/lnaiarasantos/com/br/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package lnaiarasantos.com.br;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
